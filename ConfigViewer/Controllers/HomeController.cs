33043bfb2cd45f1d3042647c4cee56e318901831
Kevin Gysberg <kegysber@microsoft.com>
2019-08-20 21:54:33 -0400

    Changed home controller to spit out all aspnetcore config values

--- ConfigViewer/Controllers/HomeController.cs
+++ ConfigViewer/Controllers/HomeController.cs
@@ -5,14 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ConfigViewer.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace ConfigViewer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _config;
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_config.AsEnumerable());
         }
 
         public IActionResult Privacy()
